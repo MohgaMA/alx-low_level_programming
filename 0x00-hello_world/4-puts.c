2db9df926a81716e93451bb1c15c9da232046297
ashour <mohga.m.ashour@gmail.com>
2023-06-18 13:12:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+ * the entrary point of program returns 0
+ * the program prints a string
+ *\
+
 int main(void)
 	{
 	puts("\"Programming is like building a multilingual puzzle");
