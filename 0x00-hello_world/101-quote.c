bbf763fcf509b1986e18115d34f6e7776eb52c8e
ashour <mohga.m.ashour@gmail.com>
2023-06-18 17:19:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <stdin.h>
 /**
  * main - Block of code
  * Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdio.h);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdin.h);
 	return (1);
 }
