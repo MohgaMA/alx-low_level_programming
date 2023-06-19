575b550f357352e30e6b08271174802ec8223a75
ashour <mohga.m.ashour@gmail.com>
2023-06-18 18:28:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	FILE *printf ;
 	printf = fopen("file.text", "w");
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	fputs(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", printf);
 	fclose(printf);
 	return (1);
 }
