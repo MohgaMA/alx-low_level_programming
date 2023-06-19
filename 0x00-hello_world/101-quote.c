9941c01c437c1a36170e66f1da330c74219d2a9d
ashour <mohga.m.ashour@gmail.com>
2023-06-18 17:17:13 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Block of code
+ * Return: 1
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdio.h);
+	return (1);
+}
