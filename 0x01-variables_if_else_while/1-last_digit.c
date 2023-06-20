49f523b16762e66f3d1b82b0a5baf4a4502e706c
ashour <mohga.m.ashour@gmail.com>
2023-06-19 19:07:45 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,10 +19,10 @@ int main(void)
 	/* your code goes there */
 	last = n % 10;
 	if (last > 5)
-		printf("The string %d of %d and is greater than 5\n", Last, n);
+		printf("The string %d of %d and is greater than 5\n", last, n);
 	if (last == 0)
-		printf("The string %d of %d and is 0\n", Last, n);
+		printf("The string %d of %d and is 0\n", last, n);
 	if (last < 6 && n != 0)
-		printf("The string %d of %d and is less than 6 and not 0\n", Last, n);
+		printf("The string %d of %d and is less than 6 and not 0\n", last, n);
 	return (0);
 }
