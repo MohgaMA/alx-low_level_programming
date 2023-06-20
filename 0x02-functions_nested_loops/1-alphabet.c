996200e3943f507943479f796900f781445870c4
ashour <mohga.m.ashour@gmail.com>
2023-06-20 13:30:31 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_alphabet - the main block of code
+ * Return: 0
+ * DEscription: function print lowercase alphabet
+ */
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
