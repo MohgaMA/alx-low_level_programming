3833ac26fc1c491e912108c9a859ae2e5d9232e8
ashour <mohga.m.ashour@gmail.com>
2023-06-22 01:52:38 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - block of code
+ * Return: 0
+ */
+int main(void)
+{
+	int c[2] = {0, 1};
+
+	while ((c[0] && c[1] < 9) || (c[0] && c[1] == 9))
+	{
+		putchar(c[0] + '0');/* to print the c value*/
+		putchar(c[1] + '0');
+
+		if (c[0] != 9 && c[1] == 9)/* to define when the , and space will be put*/
+		{
+			if (c[0] == c[1])/*to make sure the repeted digit will not be printed*/
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+		c[1]++;
+	}
+	putchar('\n');
+	return (0);
+}
