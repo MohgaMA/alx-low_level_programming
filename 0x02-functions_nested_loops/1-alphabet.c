0f8ced4ce9eca17e90bef524c41a11656ecf1c1b
ashour <mohga.m.ashour@gmail.com>
2023-06-25 14:48:30 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,17 +5,18 @@
  * Return:0
  */
 
-void print_alphabet(void)
+int alphabet(void)
 {
 	char c = 'a';
 
 	while (c <= 'z')
 	{
-		_putchar(c);
+		putchar(c);
 		c++;
 
 	}
-	_putchar('\n');
+	putchar('\n');
+	return (0);
 
 }
 
