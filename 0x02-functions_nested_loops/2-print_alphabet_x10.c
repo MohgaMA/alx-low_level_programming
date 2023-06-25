71fcf7a7762c4a8f6032de66a30136a4531318ad
ashour <mohga.m.ashour@gmail.com>
2023-06-25 15:31:46 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * main - the main function
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		char c = 'c';
+		while (c <= 'z')
+		{
+			_putchar('c');
+		}
+		c = 0;
+	}
+}
