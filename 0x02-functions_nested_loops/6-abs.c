49cf102a737978fcf2d45888565b2fcd9f2e456a
ashour <mohga.m.ashour@gmail.com>
2023-07-02 18:16:03 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _abs - prints the absolute value of a number
+*@m: number targeted
+*Return: returns 0
+*/
+int _abs(int m)
+{
+	if (m < 0)
+	{
+		m = m + -m;
+		_putchar('m');
+	}
+	return (0);
+}
