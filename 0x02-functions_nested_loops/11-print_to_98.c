a0dab1a6eda93a84e3372cca28a95269fb5312ba
ashour <mohga.m.ashour@gmail.com>
2023-07-02 21:24:31 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@
 */
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n >= 98)
 	{
 		while (n > 98)
 		{
@@ -17,9 +17,9 @@ void print_to_98(int n)
 		}
 		printf("%d", 98);
 	}
-	else if (n < 98)
+	else if (n <= 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
