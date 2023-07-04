f26f576c047e729a192054206a4744ae2e577fdd
ashour <mohga.m.ashour@gmail.com>
2023-07-04 00:08:00 -0700

    edit the code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,6 +34,12 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((table % 10) + '0');
 				}
+				else if (table >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
 				else
 				{
 					_putchar(table % 10 + '0');
