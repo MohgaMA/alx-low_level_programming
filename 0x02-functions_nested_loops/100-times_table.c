7a58cb030d8a6dee1198d754183cf819b58c1d4e
ashour <mohga.m.ashour@gmail.com>
2023-07-03 23:51:05 -0700

    edit the code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,13 +10,13 @@ void print_times_table(int n)
 	if (n < 15 && n > 0)
 	{
 		int f, s = 0;
-		
+
 		for (f = 0 ; f <= 9 ; f++)
 		{
-			for (s = 0 ; s <= 9 ; s++)
+			for (s = 0 ; s <= n ; s++)
 			{
 				int table = s * f;
-				
+
 				if (s != 0)
 				{
 					_putchar(',');
