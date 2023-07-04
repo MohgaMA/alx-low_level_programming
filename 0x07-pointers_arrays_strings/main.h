2cf7abddf7b36139430643ee06ff305174c070fa
ashour <mohga.m.ashour@gmail.com>
2023-07-03 19:58:08 -0700

    pointer

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
