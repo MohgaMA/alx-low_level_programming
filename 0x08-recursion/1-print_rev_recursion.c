4d19d4b1a757c508816d2399e3db7edc84595604
ashour <mohga.m.ashour@gmail.com>
2023-07-06 19:26:53 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,12 +6,17 @@
 */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar(s[*s - 1]);
 		_print_rev_recursion(s - 1);
+
+		if (*s == '\0')
+		{
+			_print_rev_recursion(s + 1);
+			_putchar(s[0]);
+		}
 	}
-	else 
+	else
 		_putchar('\n');
 }
 
