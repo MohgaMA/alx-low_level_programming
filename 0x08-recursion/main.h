56c362eac04e7ccef367b722fe7a295f87d5768b
ashour <mohga.m.ashour@gmail.com>
2023-07-06 18:45:02 -0700

    puts

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,10 +2,7 @@
 /*
  * header files
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
