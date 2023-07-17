10cc71ac8dfdb806d33581869736eda6ffaf83ed
ashour <mohga.m.ashour@gmail.com>
2023-07-17 12:49:42 -0700

    string

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -27,14 +27,13 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		for (l = 0; av[i][l] != '\0'; l++)
+		for (l = 0; av[i][l]; l++)
 		{
 			str[k] = av[i][l];
 			k++;
-			str[k++] = '\n';
 		}
-		/*if (str[k] != '\0')
-			str[k++] = '\n';*/
+		if (str[k] != '\0')
+			str[k++] = '\n';
 	}
 	return (str);
 }
