84ec72dd9970796ce70360c6e26ba76d96bdd12b
ashour <mohga.m.ashour@gmail.com>
2023-07-28 08:07:00 -0700

    list

--- 0-print_list.c
+++ 0-print_list.c
@@ -12,12 +12,10 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		h.len = strlen(h.str);
-
-		if (h.str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)");
 		else
-			printf("[%d] %s", h.len,  h.str);
+			printf("[%d] %s", h->len,  h->str);
 		i++;
 	}
 	return (i);
