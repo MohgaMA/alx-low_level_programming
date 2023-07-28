b67f253ed5491b14996be4d9b3fd4a84a8c4dde9
ashour <mohga.m.ashour@gmail.com>
2023-07-28 13:13:57 -0700

    lenth of node

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,25 +1,23 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * print_list - Prints all the elements of a list_t list.
+ * list_len - Prints all the node of list_t list.
  * @h: The list_t list.
  *
  * Return: The number of nodes in h.
  */
 size_t list_len(const list_t *h)
 {
-        size_t nodes = 0;
-
-        while (h)
-        {
+	size_t nodes = 0;
+	
+	while (h)
+	{
 		if (h->str == null)
-			return(NULL);
+			printf("[0] (nil)\n");
 		else
-                        printf("%d elements", h->len);
-
-                nodes++;
-                h = h->next;
-        }
-
-        return (nodes);
+			printf("%d elements\n", h->len);
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
 }
