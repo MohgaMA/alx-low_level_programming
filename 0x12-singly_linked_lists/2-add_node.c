211b96f8a6789f4d986f2c969ae16bfac9af7351
ashour <mohga.m.ashour@gmail.com>
2023-07-28 18:08:46 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,20 +14,22 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newnode;
+	char *dup;
 	unsigned int len;
 
+	dup = strdup(str);
 	newnode = malloc(sizeof(list_t));
 
 	if (newnode == NULL)
 		return (NULL);
 	for (len = 0; str[len]; len++)
 		;
-	if (newnode->str == NULL)
+	if (dup == NULL)
 	{
 		free(newnode);
 		return (NULL);
 	}
-	newnode->str = strdup(str);
+	newnode->str = dup;
 	newnode->len = len;
 	newnode->next = *head;
 
