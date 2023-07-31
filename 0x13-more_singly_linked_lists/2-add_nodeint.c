a20192767d122d13d356bbd048d929d6b6208200
ashour <mohga.m.ashour@gmail.com>
2023-07-31 13:12:18 -0700

    add node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,19 @@
+/**
+ * add_nodeint_end - add a new node at the end
+ * of a linked list
+ * @head: head of a list.
+ * @n: n element.
+ *
+ * Return: address of the new element. NUll if it failed.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	 listint_t *new;
+
+	 new = malloc(sizeof(listint_t));
+	 new->n = n;
+	 new->next = *head;
+	 *head = new;
+
+	return (*head);
+}
