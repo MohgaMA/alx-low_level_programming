a66843d60a4bd5f56bcbb193577822f26312e07b
ashour <mohga.m.ashour@gmail.com>
2023-07-31 12:39:36 -0700

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t node;
-	unsigned int len = 0;
 
 	while (h)
 	{
-		len++;
+		node++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return (len);
+	return (node);
 }
