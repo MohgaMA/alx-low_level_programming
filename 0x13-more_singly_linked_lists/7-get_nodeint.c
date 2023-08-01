399557435e0bc78726d63f1189776d40171a537c
ashour <mohga.m.ashour@gmail.com>
2023-07-31 21:25:37 -0700

    git nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		if (i == index)
 		{
 			head = head->next;
+			return (head);
 		}
-		return (NULL);
 	}
+	return (NULL);
 }
