acf8b6d9f47d4769f367673af30e85609a5d82f8
ashour <mohga.m.ashour@gmail.com>
2023-08-01 11:16:10 -0700

    return node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,12 +11,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	for (i = 0; head != NULL; i++)
+	for (i = 0; head != NULL && i < index; i++)
 	{
-		if (i == index)
-		{
-			head = head->next;
-		}
+		head = head->next;
 	}
 	return (head);
 }
