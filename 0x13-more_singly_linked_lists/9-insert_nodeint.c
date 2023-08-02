952263448d22a601d5a59ee4de841cc7f187b5e3
ashour <mohga.m.ashour@gmail.com>
2023-08-01 17:39:43 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (i = 0; current != NULL; i++)
 		current = current->next;
-	if (i == idx)
+	if (idx == 0)
 	{
 		newnode->next = *head;
 		*head = newnode;
