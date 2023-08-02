899d41425afb57f6a569c76f59c14e047051598a
ashour <mohga.m.ashour@gmail.com>
2023-08-01 17:37:02 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (*head == NULL || head == NULL)
 		return (NULL);
-	if (ndx != 0 && head == NULL)
+	if (idx != 0 && head == NULL)
 		return (NULL);
 
 	current = *head;
