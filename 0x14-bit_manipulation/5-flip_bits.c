823db8c3df82cdf289fa65d6702f15bb732a197f
ashour <mohga.m.ashour@gmail.com>
2023-08-04 19:38:49 -0700

    update

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,15 +7,13 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int flips = 0;
-	unsigned long int xor = (n ^ m);
-	unsigned long int max = 0x01;
+	unsigned long int xor = n ^ m, bits = 0;
 
-	while (max <= xor)
+	while (xor > 0)
 	{
-		if (max & xor)
-			flips++;
-		max <<= 1;
+		bits += (xor & 1);
+		xor >>= 1;
 	}
-	return (flips);
+
+	return (bits);
 }
