00e5cea7ed16011c35315c1d1297d65b10249e07
ashour <mohga.m.ashour@gmail.com>
2023-08-14 14:06:55 -0700

    natural number

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - prints count of multiples
+*of 3 or 5 below 1024
+*Return: return 0
+*/
+int main(void)
+{
+	int sum = 0, i;
+
+	for (i = 0; i != 1024; i += 3)
+	{
+		sum = sum + i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
