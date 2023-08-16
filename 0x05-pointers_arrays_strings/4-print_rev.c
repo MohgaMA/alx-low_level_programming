bd58d948169ac9fb0a38ca5e6af95dc5495540a1
ashour <mohga.m.ashour@gmail.com>
2023-08-16 13:50:44 -0700

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,3 +16,16 @@ void print_rev(char *s)
 	}
 	_putchar('\n');
 }
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
+int _strlen(char *s)
+{
+        int i = 0;
+
+        while (s[i] != '\0')
+                i++;
+        return (i);
+}
