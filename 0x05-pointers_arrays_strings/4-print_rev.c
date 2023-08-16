c1af7216a14860c05ce4ddc0f3ea927fd42c2e0f
ashour <mohga.m.ashour@gmail.com>
2023-08-16 13:45:17 -0700

    revv

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,10 +6,13 @@
 
 void print_rev(char *s)
 {
-	int len, i;
+	int len;
 
 	len = _strlen(s);
-	for (len; len != 0; len--)
+	while (len != 0)
+	{
 		_putchar(s[len]);
+		len--;
+	}
 	_putchar('\n');
 }
