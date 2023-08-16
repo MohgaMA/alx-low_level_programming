cd1dd40ae621fbfd5cd1cf33ee413e621c52a6a9
ashour <mohga.m.ashour@gmail.com>
2023-08-16 13:36:13 -0700

    make the putchar

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,17 @@
 #include <unistd.h>
-/*
- * header files
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
