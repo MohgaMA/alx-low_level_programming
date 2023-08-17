4d726f585bbf00db0c74c219c82c9bcb985ddfaa
ashour <mohga.m.ashour@gmail.com>
2023-08-17 13:04:54 -0700

    string rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,15 +6,17 @@
 */
 void rev_string(char *s)
 {
-	 int len;
+	int len = 0, i = 0;
+	char *c;
 
-        len = _strlen(s);
-        while (len != 0)
-        {
-                _putchar(s[len - 1]);
-                len--;
-        }
-        _putchar('\n');
+	len = _strlen(s);
+	while (len != 0)
+	{
+		c[i++] = s[len - 1];
+		len--;
+	}
+	*s = *c;
+
 }
 /**
  *_strlen - returns the length of a string
@@ -23,9 +25,9 @@ void rev_string(char *s)
  */
 int _strlen(char *s)
 {
-        int i = 0;
-
-        while (s[i] != '\0')
-                i++;
-        return (i);
+	int i = 0;
+	
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
