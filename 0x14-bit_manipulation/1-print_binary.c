8b3361f82a6dd02b3b1be583061e14d79ef63da5
MohgaMA <mohga.m.ashour@gmail.com>
2023-09-03 16:10:55 -0700

    new solution

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,8 +6,14 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
+	unsigned long int res, c;
 
-	_putchar((n & 1) + '0');
+	for (c = 63; c >= 1; c--)
+	{
+		res = n >> c - 1;
+		if (n & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
 }
